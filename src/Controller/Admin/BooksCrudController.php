598c982907a2b11e1691ffc560347e7e16967371
Skencker <Sandrinekencker@hotmail.com>
2021-10-16 03:25:06 +0200

    update crud books

--- src/Controller/Admin/BooksCrudController.php
+++ src/Controller/Admin/BooksCrudController.php
@@ -4,6 +4,13 @@ namespace App\Controller\Admin;
 
 use App\Entity\Books;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class BooksCrudController extends AbstractCrudController
 {
@@ -12,14 +19,24 @@ class BooksCrudController extends AbstractCrudController
         return Books::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            BooleanField::new('status','Disponibilité'),
+            TextField::new('name','Nom'),
+            SlugField::new('slug')->setTargetFieldName('name'),
+            TextField::new('author', 'Auteur'),
+            TextareaField::new('description','Déscription'),
+            DateField::new('datePub', 'Date de publication'),
+            AssociationField::new('category','Catégorie'),
+            AssociationField::new('genre','Genre'),
+            ImageField::new('image')
+                ->setUploadDir('public/uploads/images')
+                ->setBasePath('uploads/images')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setRequired(false)
         ];
     }
-    */
+    
 }
--- src/Controller/Admin/BorrowDetailsCrudController.php
+++ src/Controller/Admin/BorrowDetailsCrudController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Admin;
 
 use App\Entity\BorrowDetails;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class BorrowDetailsCrudController extends AbstractCrudController
 {
@@ -12,14 +14,14 @@ class BorrowDetailsCrudController extends AbstractCrudController
         return BorrowDetails::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('borrow','Emprunt'),
+            TextField::new('book', 'Livres')
         ];
     }
-    */
+    
 }
+
