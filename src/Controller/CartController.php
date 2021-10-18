6122d5525cab4aa1160eae1ea5611a1de97c5e7c
Skencker <Sandrinekencker@hotmail.com>
2021-10-18 10:45:23 +0200

    update cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -17,10 +17,10 @@ class CartController extends AbstractController
     
     public function index(Cart $cart): Response
     {
-        if (count($cart->get()) == 5) {
+        // if (count($cart->get()) == 5) {
 
-            $this->addFlash('cartfull', 'Vous avez atteints le nombre de livre maximun à emptrunter (5).');
-        }
+        //     $this->addFlash('cartfull', 'Vous avez atteints le nombre de livre maximun à emptrunter (5).');
+        // }
         
         return $this->render('cart/index.html.twig', [
             'cart' => $cart->getFull()
